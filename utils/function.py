18a35247abb2574b4fe6c0b7aa0bbe03d18229df
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2025-12-22 22:04:23 -0500

    compilation, architectures and graphics

--- utils/function.py
+++ utils/function.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-# Grupo de Biología Computacional, Universidad de Antioquia, Medellín, Colombia
-"""
-"""
 
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+from sklearn.metrics import auc
 from Bio import SeqIO
 from pathlib import Path
-
-
-#from utils import converter as cv
 from . import converter as cv 
-#import converter as cv
 from itertools import product
 from sklearn.utils import shuffle
-
-import tensorflow as tf
 from keras.utils import to_categorical
 from keras import backend as K
 from sklearn.metrics import f1_score, confusion_matrix, ConfusionMatrixDisplay, roc_curve, roc_auc_score
@@ -23,6 +19,7 @@ from keras.models import Sequential
 from keras.layers import Input, Conv2D, Conv1D, MaxPooling2D, MaxPooling1D, Flatten, Dense, Dropout, BatchNormalization, Activation
 from keras.regularizers import l1, l2, l1_l2
 from keras.optimizers import Adam
+from sklearn.model_selection import KFold
 
 
 def fasta_to_onehot(pos_path: str, neg_path: str) -> pd.DataFrame:
@@ -65,107 +62,78 @@ def fasta_to_onehot(pos_path: str, neg_path: str) -> pd.DataFrame:
     neg = read_fasta(neg_path, 0)
 
     df = pd.DataFrame(pos + neg, columns=['header', 'sequence', 'encoding', 'label'])
-    df = pd.sample(frac=1, random_state=42).reset_index(drop=True)
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
     return df
 
 
+def training_process(df, n_folds, cnns):
 
-def training_process(X_train, y_train, n_folds, representation):
+    X = np.array(df['encoding'].tolist())
+    y = df['label'].values
 
     kf = KFold(n_splits=n_folds, shuffle=True, random_state=42)
+    input_shape = X.shape[1:]
 
-    # List of CNN architectures
-    cnns = [DeepSplicer_regu, Spliceator, SpliceFinder]
+    evaluation_results = {cnn.__name__: [] for cnn in cnns}
+    training_histories = {cnn.__name__: [] for cnn in cnns}
+    roc_curves = {cnn.__name__: [] for cnn in cnns}
+    confusion_matrixs = {cnn.__name__: [] for cnn in cnns}
 
-      
-    # Dictionary to hold results for each CNN
-    results_dict = {cnn.__name__: [] for cnn in cnns}
+    for cnn in cnns:
+        print(f'\n--- Training {cnn.__name__} ---')
 
-    # Adjust input shape if necessary
-    input_shape = X_train.shape[1:]
-    if input_shape == (1024,):
-        input_shape = (1024, 1)
+        fold_metrics = []
+        all_true = []
+        all_pred = []
 
-    early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+        for fold, (train_index, val_index) in enumerate(kf.split(X)):
+            print(f'\nFold {fold+1}/{n_folds}')
+        
+            X_train_fold, X_val_fold = X[train_index], X[val_index]
+            y_train_fold, y_val_fold = y[train_index], y[val_index]
 
-    # Perform k-fold cross-validation over each CNN architecture
-    for train_index, val_index in kf.split(X_train):
-           
-        X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
-        y_train_fold, y_val_fold = y_train[train_index], y_train[val_index]
-
-        for cnn in cnns:
-            epochs = 150
             model = cnn(input_shape)
-            history = model.fit(X_train_fold, y_train_fold, epochs=epochs, batch_size=32, validation_data=(X_val_fold, y_val_fold), 
-                      callbacks=[early_stop], verbose=1)
-            trained_epochs = len(history.epoch)
+            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'precision', 'recall', 'auc'])
+            history = model.fit(X_train_fold, y_train_fold, epochs=2, batch_size=32, validation_data=(X_val_fold, y_val_fold), verbose=1)
             
-            results = model.evaluate(X_val_fold, y_val_fold) #verbose=0
+            training_histories[cnn.__name__].append(history.history)
 
-            # Make predictions and calculate F1 score
-            y_val_pred = model.predict(X_val_fold, verbose =0)
-            y_val_pred_classes = np.argmax(y_val_pred, axis=1)
-            f1 = f1_score(np.argmax(y_val_fold, axis=1), y_val_pred_classes, average='weighted')
+            loss, accuracy, precision, recall, auc = model.evaluate(X_val_fold, y_val_fold, verbose=0)
 
-            # Store results for the current fold
-            results_dict[cnn.__name__].append([trained_epochs] + list(results) + [f1])
-            
-    # Ad all results
-    all_results = []
-    for model_name, metrics in results_dict.items():
-        for fold_metrics in metrics:
-            all_results.append([model_name]+fold_metrics)
-       
-    df_all_results = pd.DataFrame(all_results, columns=['architecture', 'epochs_trained','loss', 'accuracy', 'precision', 'recall', 'f1_score'])
+            y_pred_prob = model.predict(X_val_fold)
+            y_pred = (y_pred_prob > 0.5).astype(int)
 
-    return df_all_results
+            all_true.extend(y_val_fold)
+            all_pred.extend(y_pred)
 
+            f1 = f1_score(y_val_fold, y_pred, average='weighted')
 
+            fold_metrics.append([loss, accuracy, precision, recall, auc, f1])
 
-def load_data(train_neg, train_pos):
+            fpr, tpr, _ = roc_curve(y_val_fold, y_pred_prob) 
+            roc_curves[cnn.__name__].append((fpr, tpr))
 
-    # Concatenate the DataFrames df_negative and df_positive into a single DataFrame
-    train_data = pd.concat([train_neg, train_pos], ignore_index = True)
-    train_data = shuffle(train_data)
-    #test_data = pd.concat([test_neg, test_pos], ignore_index = True)
-  
-    # Getting lists from the Data Frame
-    sequences_list_train = train_data['encoding'].to_list()
-    labels_list_train = train_data['label'].to_list() 
+        cm = confusion_matrix(all_true, all_pred)
+        confusion_matrixs[cnn.__name__].append(cm)
+      
+        # Guardar el promedio del modelo
+        avg_loss = np.mean([m[0] for m in fold_metrics])
+        avg_acc  = np.mean([m[1] for m in fold_metrics])
+        avg_preci   = np.mean([m[2] for m in fold_metrics])
+        avg_recall = np.mean([m[3] for m in fold_metrics])
+        avg_auc  = np.mean([m[4] for m in fold_metrics])
+        avg_f1   = np.mean([m[5] for m in fold_metrics])
 
-    # Transforming lists to Numpy Arrays and redimensioning features array
-    X_train = np.array(sequences_list_train)
-    y_train = np.array(labels_list_train)
-    
-    # Transforming labels in one-hot codification 
-    y_train = to_categorical(y_train, num_classes=2)
- 
-    return np.array(X_train),np.array(y_train)
+        evaluation_results[cnn.__name__] = [avg_loss, avg_acc, avg_preci, avg_recall, avg_auc, avg_f1]
 
-def f1_metric(y_true, y_pred):
-    
-    # Convert predictions in classes
-    y_pred_classes = K.argmax(y_pred, axis=-1)
-    y_true_classes = K.argmax(y_true, axis=-1)
-    
-    # Calculate F1 Score
-    return f1_score(y_true_classes, y_pred_classes, average='weighted')
+    df_results = pd.DataFrame.from_dict(evaluation_results, orient='index', columns=['loss', 'accuracy', 'precision', 'recall', 'auc', 'f1']).reset_index().rename(columns={'index': 'cnn'})
 
-def recall_m(y_true, y_pred):
-    true_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_true * y_pred, 0, 1)))
-    possible_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-def precision_m(y_true, y_pred):
-    true_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_true * y_pred, 0, 1)))
-    predicted_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-
+    return {'evaluation_metrics': df_results, 
+            'training_curves': training_histories,
+             'roc_curves': roc_curves,
+             'confusion_matrices': confusion_matrixs
+               }
 
 def SpliceFinder(input_shape): #SpliceFinder - https://link.springer.com/article/10.1186/s12859-022-04971-w#Sec2 
 
@@ -177,41 +145,19 @@ def SpliceFinder(input_shape): #SpliceFinder - https://link.springer.com/article
         Dense(100, activation='relu'),
         Dropout(0.3),
         
-        Dense(2, activation='sigmoid')  
+        Dense(1, activation='sigmoid')  
     ])
-
-    model.compile(optimizer=Adam(learning_rate=1e-4),
-                  loss='binary_crossentropy', 
-                  metrics=[tf.keras.metrics.Precision(),'accuracy', tf.keras.metrics.Recall()])
 
     return model                
 
 
-def DeepSplicer_regu(input_shape):
+def DeepSplicer(input_shape):
 
     model = Sequential([
     # First block of normalization
     BatchNormalization(input_shape=input_shape),
     Conv1D(filters=16, kernel_size=11, padding='same', kernel_regularizer=l1(0.0001)),
     Activation('relu'), 
-    MaxPooling1D(pool_size=2),
-    Dropout(0.2),
-    
-    BatchNormalization(),
-    Conv1D(filters=32, kernel_size=11, padding='same', kernel_regularizer=l1(0.0001)),
-    Activation('relu'),
-    MaxPooling1D(pool_size=2),
-    Dropout(0.2),
-        
-    BatchNormalization(),
-    Conv1D(filters=64, kernel_size=21, padding='same', kernel_regularizer=l1_l2(l1=0.0001, l2=0.0001)),
-    Activation('relu'),
-    MaxPooling1D(pool_size=2),
-    Dropout(0.2),
-        
-    BatchNormalization(),
-    Conv1D(filters=64, kernel_size=41, padding='same', kernel_regularizer=l1_l2(l1=0.0001, l2=0.0001)),
-    Activation('relu'),
     MaxPooling1D(pool_size=2),
     Dropout(0.2),
         
@@ -221,50 +167,11 @@ def DeepSplicer_regu(input_shape):
     Dense(16, activation='relu', kernel_regularizer=l2(0.0001)),
     Dropout(0.5) ,
 
-    Dense(2, activation='sigmoid')
+    Dense(1, activation='sigmoid')
     
     ])
     
-    model.compile(optimizer=Adam(learning_rate=1e-4),
-                  loss='binary_crossentropy',
-                  weighted_metrics=[precision_m,'accuracy', recall_m ])
-    return model
 
-def DeepSplicer(input_shape):
-
-    model = Sequential([
-    # First block of normalization
-    BatchNormalization(input_shape=input_shape),
-    Conv1D(filters=16, kernel_size=11, padding='same', kernel_regularizer=l1(0.00007)),
-    Activation('relu'), 
-    MaxPooling1D(pool_size=2),
-    
-    BatchNormalization(),
-    Conv1D(filters=32, kernel_size=11, padding='same', kernel_regularizer=l1(0.00007)),
-    Activation('relu'),
-    MaxPooling1D(pool_size=2),
-        
-    BatchNormalization(),
-    Conv1D(filters=64, kernel_size=21, padding='same', kernel_regularizer=l1(0.00007)),
-    Activation('relu'),
-    MaxPooling1D(pool_size=2),
-        
-    BatchNormalization(),
-    Conv1D(filters=64, kernel_size=41, padding='same', kernel_regularizer=l1(0.00007)),
-    Activation('relu'),
-    MaxPooling1D(pool_size=2),
-        
-    Flatten(),
-    Dense(32, activation='relu'),
-    Dense(16, activation='relu'),
-        
-    Dense(2, activation='sigmoid')
-    
-    ])
-    
-    model.compile(optimizer=Adam(learning_rate=1e-4),
-                  loss='binary_crossentropy',
-                  metrics=['precision', 'accuracy', 'recall'])
     return model
     
 def Spliceator(input_shape): #https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-021-04471-3#Sec12 
@@ -286,73 +193,156 @@ def Spliceator(input_shape): #https://bmcbioinformatics.biomedcentral.com/articl
         Dense(100, activation='relu'),
         Dropout(0.2),
 
-        Dense(2, activation='sigmoid') 
+        Dense(1, activation='sigmoid') 
     ])
 
-    model.compile(optimizer=Adam(learning_rate=1e-4),
-                  loss='binary_crossentropy',  
-                  metrics=[precision_m,'accuracy', recall_m ])  
 
     return model 
 
+def plot_all_results(results, model_names, save_path="results"):
 
-    with open(pos_path, 'r', encoding='utf-8') as handle:
-        for record in SeqIO.parse(handle, 'fasta'):
-            header = record.descriotion
-            sequence = str(record.seq).upper()
+# ============================================================
+# Create directories
+# ============================================================
+    os.makedirs(f"{save_path}/curves", exist_ok=True)
+    os.makedirs(f"{save_path}/roc", exist_ok=True)
+    os.makedirs(f"{save_path}/confusion", exist_ok=True)
+    os.makedirs(f"{save_path}/metrics", exist_ok=True)
 
-            '''
-    Converts a DNA sequence into a one-hot encoded matrix
+    eval_metrics = results['evaluation_metrics']
 
-    Parameters
-    ----------
-    sequence : str
-        DNA sequence to convert
+    # ============================================================
+    # 1. CURVAS DE LOSS (PROMEDIO TRAIN + PROMEDIO VAL)
+    # ============================================================
+    plt.figure(figsize=(9,6))
 
-    Returns
-    -------
-    numpy.ndarray
-        One-hot encoded matrix of shape (len(sequence), 4)
-    '''
-    seq_data = []
+    colors = plt.cm.tab10(np.linspace(0, 1, len(model_names)))
 
-    # Define encoding for each nucleotide
-    encoding = {
-        'A': [1, 0, 0, 0, 0],
-        'C': [0, 1, 0, 0, 0],
-        'G': [0, 0, 1, 0, 0],
-        'T': [0, 0, 0, 1, 0],
-        'N': [0, 0, 0, 0, 0]  # Encoding for unknown nucleotides
-    }
+    for i, model_name in enumerate(model_names):
+
+        histories = results['training_curves'][model_name]
     
-    # Convert each nucleotide to its one-hot encoding
-    seq_data = [encoding.get(nucleotide, encoding['N']) for nucleotide in sequence]
+        all_train_loss = np.array([h['loss'] for h in histories])
+        all_val_loss = np.array([h['val_loss'] for h in histories])
+        
+        # compute mean through folds
+        mean_train_loss = all_train_loss.mean(axis=0)
+        mean_val_loss = all_val_loss.mean(axis=0)
+
+        epochs = range(1, len(mean_train_loss) + 1)
+
+        plt.plot(
+            epochs,
+            mean_train_loss,
+            label=f"{model_name} Train",
+            color=colors[i],
+            linestyle="-",
+            linewidth=2
+        )
+        plt.plot(
+            epochs,
+            mean_val_loss,
+            label=f"{model_name} Val",
+            color=colors[i],
+            linestyle="--",
+            linewidth=2
+        )
+
+    plt.title("Average Loss Curves Across Models")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+
+    plt.savefig(f"{save_path}/curves/all_models_loss.png", dpi=300)
+    plt.close()
+
+    # ============================================================
+    # 2. ROC CURVES
+    # ============================================================
     
-    # Convert list to numpy array
- 
-    return np.array(seq_data)
+    plt.figure(figsize=(7,7))
 
-            oh_rep = cv.onehotmatrix
+    for model_name in model_names:
+
+        rocs = results['roc_curves'][model_name]
+
+        mean_fpr = np.linspace(0,1,100)
+        interpolated_tprs = []
+
+        for fpr, tpr in rocs:
+            interp = np.interp(mean_fpr, fpr, tpr)
+            interpolated_tprs.append(interp)
+
+        mean_tpr = np.mean(interpolated_tprs, axis=0)
+        model_auc = auc(mean_fpr, mean_tpr)
+
+        plt.plot(mean_fpr, mean_tpr, linewidth=2, label= f'{model_name} AUC = {model_auc:.3f}')
+
+    plt.plot([0,1], [0,1], '--', color='gray') 
+    plt.title("Average ROC Curves Across Models")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.savefig(f"{save_path}/roc/all_models_roc.png", dpi=300)
+    plt.close()
+
+    
+    # ============================================================
+    # 3. METRICS BARPLOT (COMPARING MODELS)
+    # ============================================================
+    metrics = ["accuracy", "precision", "recall", "auc", "f1"]
+    
+    values = np.array([
+         [
+            eval_metrics.loc[eval_metrics["cnn"] == model, metric].values[0]
+            for model in model_names
+        ]
+        for metric in metrics
+    ])
+
+    x = np.arange(len(metrics))
+    width = 0.8 / len(model_names)
+    
+    plt.figure(figsize=(10,6))
+    
+    for i, model_name in enumerate(model_names):
+        plt.bar(x + i*width, values[:,i], width=width, label=model_name)
+
+    plt.xticks(x + width*(len(model_names)-1)/2, metrics)
+    plt.ylim(0,1)
+    plt.title("Average Metrics Across Models")
+    plt.ylabel("Score")
+    plt.legend(title='Models')
+    plt.grid(axis="y", alpha=0.3)
+    plt.tight_layout()
+
+    plt.savefig(f"{save_path}/metrics/all_models_metrics.png", dpi=300)
+    plt.close()
 
 
 
+    # ============================================================
+    # 4. CONFUSION MATRICES (ONE PER MODEL)
+    # ============================================================
+    for model_name in model_names:
 
-def fasta2df_onehot(filename, type_label=True):
+        conf_mats = results["confusion_matrices"][model_name]
+        total_cm = np.sum(conf_mats, axis=0)
 
-    df = pd.DataFrame(columns=['header', 'sequence', 'encoding', 'label'])
+        plt.figure(figsize=(6,5))
+        sns.heatmap(total_cm, annot=True, fmt="d", cmap="Blues")
 
-    with open(filename) as handle:
-        for record in SeqIO.parse(handle, 'fasta'):
-            header = record.description.title()
-            sequence = str(record.seq.upper())
-            oh_rep = cv.onehotmatrix(record.seq.upper())
+        plt.title(f"Confusion Matrix - {model_name}")
+        plt.xlabel("Predicted")
+        plt.ylabel("True")
+        plt.tight_layout()
 
-            if type_label:
-                label = 1
-            else:
-                label = 0
+        plt.savefig(f"{save_path}/confusion/{model_name}_confusion.png", dpi=300)
+        plt.close()
 
-            df.loc[len(df)] = [header, sequence, oh_rep, label]
-    #df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-    return df
-{}
+    print(f"\n✨ All plots were successfully generated in '{save_path}/' folder!\n")
